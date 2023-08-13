cf78db7f90f71e90a46f2bd2e9c0953aabb38024
davidcoats <david@getsafetycone.com>
2023-08-12 23:24:50 -0700

    Interim changes.

--- source/D8S.R0003/Components/Links/GorillaPerceptionFailed.razor.cs
+++ source/D8S.R0003/Components/Links/GorillaPerceptionFailed.razor.cs
@@ -1,15 +1,16 @@
 using System;
 
-using R5T.T0154;
+using R5T.T0223;
 
 
 namespace D8S.R0003
 {
     /// <summary>
-    /// YouTube link to the interesting 1999 psychology study showing that when people are focused on counting basketball passes while watching a video, they don't notice a man in a gorilla suit walk right through the scene!
+    /// YouTube link to the interesting 1999 psychology study showing that when people are focused on counting basketball passes while watching a video,
+    /// they don't notice a man in a gorilla suit walk right through the scene!
     /// </summary>
-    [RazorComponentMarker]
-    public partial class GorillaPerceptionFailed : IRazorComponentMarker
+    [RazorLinkMarker]
+    public partial class GorillaPerceptionFailed
     {
     }
 }
